6cfe2b4babf937faf782bcd41631bbbfa148ec8e
David Hu <david.hu@mail.mcgill.ca>
2021-03-06 00:02:52 -0500

    constructor

--- gitTest/src/git/test/Student.java
+++ gitTest/src/git/test/Student.java
@@ -1,5 +1,17 @@
 package git.test;
 
 public class Student {
-
+	int id;
+	int name;
+	public int getid() {
+		return id;
+	}
+	public int getName() {
+		return name;
+	}
+	
+	public Student (int id, int name) {
+		this.id = id;
+		this.name = name;
+	}
 }
